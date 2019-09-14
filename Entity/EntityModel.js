511115166218f2bb114826bcc3c416bb3a6952d4
Richard Batista <RBatista@NYCCFB.INFO>
2019-09-14 13:28:53 -0400

    update

--- Entity/EntityModel.js
+++ Entity/EntityModel.js
@@ -15,34 +15,43 @@ class EntityModel {
 
     type(type){
         this.obj.type = type;
+        return this;
     }
     // is this obj a primary key
     primary(){
         this.obj.primary = true;
+        return this;
     }
+
     // sets the default value in the DB
     default(value){
         this.obj.default = value;
+        return this;
     }
 
     required(){
         this.obj.required = true;
+        return this;
     }
 
     get(func){
         this.obj.get = func;
+        return this;
     }
 
     virtual(){
         this.obj.virtual = true;
+        return this;
     }
 
     hasMany(name){
         this.obj.hasMany = name;
+        return this;
     }
     
     belongsTo(){
         this.obj.belongsTo = true;
+        return this;
     }
 
 }
--- Entity/EntityTrackerModel.js
+++ Entity/EntityTrackerModel.js
@@ -1,35 +1,40 @@
 class EntityTrackerModel {
-    constructor (entity) {
+    constructor () {
         this.__ID = Math.floor((Math.random() * 100000) + 1);;
         this.__dirtyFields = [];
         this.__state = "track";
-        this.__entity = entity;
-        this.__name = entity.__name;
+        this.__entity = null;
+        this.__name = null;
     }
 
     // entity states https://docs.microsoft.com/en-us/dotnet/api/system.data.entitystate?view=netframework-4.7.2
 
     // start tracking model
-    static build(modelValue, currentEntity){
+    build(modelValue, currentEntity){
+        this.__proto__ = {};
         const modelFields = Object.entries(modelValue); /// return array of objects
-        var modelClass = new EntityTrackerModel(currentEntity);
-    
-        for (const [modelField, modelFieldValue] of modelFields) {
+        var modelClass = this; // build entity with models
+        modelClass.__entity = currentEntity;
+        modelClass.__name = currentEntity.__name;
+
+        for (const [modelField, modelFieldValue] of modelFields) { // loop through database values
             // set the value dynamiclly
-            modelClass["_" + modelField] = modelFieldValue;
+            if(currentEntity[modelField] !== undefined){ // current entity has a value then add
+                modelClass["_" + modelField] = modelFieldValue;
 
-            // Setter
-            EntityTrackerModel.prototype.__defineSetter__(modelField, function(value){
-                modelClass.__state = "modified";
-                modelClass.__dirtyFields.push(modelField);
-                // Then it will add name to dirty fields
-                this["_" + modelField] = value;
-            });
+                // Setter
+                this.__proto__.__defineSetter__(modelField, function(value){
+                    modelClass.__state = "modified";
+                    modelClass.__dirtyFields.push(modelField);
+                    // Then it will add name to dirty fields
+                    this["_" + modelField] = value;
+                });
 
-            // Getter
-            EntityTrackerModel.prototype.__defineGetter__(modelField, function(){
-                return this["_" + modelField];
-            });
+                // Getter
+                this.__proto__.__defineGetter__(modelField, function(){
+                    return this["_" + modelField];
+                });
+            }   
         }
         return modelClass;
     }
